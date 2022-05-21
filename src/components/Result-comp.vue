5ac5da89df3a981d9bbc5e372dbfd414fe6720d3
Ifedayo Agboola <muller@cpe-172-100-0-75.twcny.res.rr.com>
2022-05-21 17:17:38 +0100

    Building a quiz app with vue

---
+++ src/components/Result-comp.vue
@@ -0,0 +1,18 @@
+<template>
+   <div class="result">
+        <div class="title">You got sample result 1!</div>
+        <div class="desc">
+            Enter a short description here about the result.
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+name:"Results-comp"
+}
+</script>
+
+<style>
+
+</style>
